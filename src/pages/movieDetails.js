ac7fd557db8fe46d7d507e636058a8ad76302af4
iKalashnikova <ikalashnikova@ukr.net>
2023-05-27 13:30:24 +0200

    details with error

--- src/pages/movieDetails.js
+++ src/pages/movieDetails.js
@@ -1,36 +1,60 @@
-import { Link, Outlet, useParams, useLocation } from "react-router-dom";
-import { useRef } from 'react'
+import { Link, Outlet, useParams, useLocation } from 'react-router-dom';
+import { useRef, useEffect, useState } from 'react';
 
 const MovieDetails = () => {
-    
-    const location = useLocation();
-    const BackLinkLocation = useRef(location.state?.from ?? '/movies')
-    console.log(location);
+  const location = useLocation();
+  const BackLinkLocation = useRef(location.state?.from ?? '/movies');
 
-    const {movieId} = useParams();
-    console.log({ movieId });
+  const { movieId } = useParams();
+  const [movieDetails, setMovieDetails] = useState(null);
 
-    
+useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNTM5NjE1NGQ2YzQ3ODZkNWQ1YmUyMTI5OWEyZmUzNiIsInN1YiI6IjY0NmY5N2IyNzcwNzAwMDExOTI0MWFkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sZAuKwEvcInwBmcDArKOc8GXVez8PoE95w65xznuwwg',
+      },
+    };
 
-    return <div> 
-        <Link to={BackLinkLocation}>Повернутись назад</Link>
-        <img src="#" alt = "#"/>
-        <h2>Назва фільму: </h2>
-        <p>рейтинг</p>
-        <h3> Overview</h3>
-        <p>опис</p>
-        <h4>Genres</h4>
-        <p>жанри</p>
-        <div> <h5>Additional information</h5>
-            <ul>
-                <li> <Link to='Cast'>Cast</Link></li>
-               <li><Link to='Reviews'>Reviews</Link></li>
-                
-            </ul>
-            <Outlet/>
-        </div>
-        
+    fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+      options
+    )
+      .then(response => response.json())
+      .then(data => setMovieDetails(data))
+      .catch(error => console.error(error));
+  }, [movieId]);
+
+//   const { title, rating, overview, genres } = movieDetails;
+
+  return (
+    <div>
+      <Link to={BackLinkLocation}>Повернутись назад</Link>
+      <img src="#" alt="#" />
+      <h2>Назва фільму: </h2>
+      <p>рейтинг</p>
+      <h3> Overview</h3>
+      <p>опис</p>
+      <h4>Genres</h4>
+      <p>жанри</p>
+      <div>
+        {' '}
+        <h5>Additional information</h5>
+        <ul>
+          <li>
+            {' '}
+            <Link to="Cast">Cast</Link>
+          </li>
+          <li>
+            <Link to="Reviews">Reviews</Link>
+          </li>
+        </ul>
+        <Outlet />
+      </div>
     </div>
-}
+  );
+};
 
 export default MovieDetails;
