b10811d5bd4deb80c248a007d2d6fd4b1434863e
iKalashnikova <ikalashnikova@ukr.net>
2023-05-27 20:40:42 +0200

    actors

--- src/components/Cast.js
+++ src/components/Cast.js
@@ -1,16 +1,39 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Cast = () => {
     const { movieId } = useParams();
+    const  [actors, setActors ] = useState([]);
 
-    // http запрос
+    useEffect(() => {
 
-    return <div>
+        const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNTM5NjE1NGQ2YzQ3ODZkNWQ1YmUyMTI5OWEyZmUzNiIsInN1YiI6IjY0NmY5N2IyNzcwNzAwMDExOTI0MWFkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sZAuKwEvcInwBmcDArKOc8GXVez8PoE95w65xznuwwg'
+  }
+};
 
-        <img src='#' alt=""/>
-        <p>Name : { movieId}</p>
-        <p>Character:</p>
-    </div>
-}
+fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`, options)
+  .then(response => response.json())
+  .then(data => setActors(data.cast))
+  .catch(err => console.error(err));
+    },[movieId])
+    
+    return (
+        <div>
+            <ul>
+                {actors.map(actor => (
+                    <li key={actor.id}>
+                        <img src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt={actor.name} />
+                        <p>Name: {actor.name}</p>
+                        <p>Character: {actor.character}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default Cast;
